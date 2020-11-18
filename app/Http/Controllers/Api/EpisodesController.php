e14aa18db2877bc4834b1fd8be60f7fc769add18
justEugene94 <drydude77@gmail.com>
2020-11-18 21:03:48 +0200

    Added EpisodesController@index

--- app/Http/Controllers/Api/EpisodesController.php
+++ app/Http/Controllers/Api/EpisodesController.php
@@ -4,7 +4,22 @@
 namespace App\Http\Controllers\Api;
 
 
-class EpisodesController
-{
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EpisodeResource;
+use App\Http\Responses\Api\Response;
+use App\Models\Episode;
 
+class EpisodesController extends Controller
+{
+    /**
+     * @return Response
+     */
+    public function index()
+    {
+        $episodes = Episode::query()->paginate(10);
+
+        $resource = EpisodeResource::collection($episodes);
+
+        return Response::make($resource, 200);
+    }
 }
---
+++ app/Http/Resources/EpisodeResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Episode;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property Episode $resource
+ */
+class EpisodeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'title' => $this->resource->title,
+            'air_date' => $this->resource->air_date,
+        ];
+    }
+}
