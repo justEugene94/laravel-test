89ba6f4f33bf17c3ee691d2c1c3769eddc7b08bf
justEugene94 <drydude77@gmail.com>
2020-11-18 15:47:01 +0200

    Added Quote model

---
+++ app/Models/Quote.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class Quote
+ * @package App\Models
+ *
+ * @property int $character_id
+ * @property string $quote
+ *
+ * Relationships:
+ * @property Character $character
+ */
+class Quote extends Model
+{
+    use HasFactory;
+
+    /** @var string  */
+    protected $table = 'quotes';
+
+    /** @var array */
+    protected $fillable = [
+        'character_id',
+        'quote',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function character()
+    {
+        return $this->belongsTo(Character::class, 'character_id', 'id');
+    }
+}
