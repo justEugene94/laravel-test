62997a08a4465c2b9d79472a72feba36546e3be9
justEugene94 <drydude77@gmail.com>
2020-11-18 15:42:18 +0200

    Added migration for Quotes

---
+++ database/migrations/2020_11_18_133744_create_quotes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('character_id');
+
+            $table->mediumText('quote');
+
+            $table->foreign('character_id')->references('id')->on('characters');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
