64d39b1ee3bdcf179529cd67b600d43dbdd933a0
justEugene94 <drydude77@gmail.com>
2020-11-19 20:33:46 +0200

    Added RegisterRequest

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -5,53 +5,24 @@ namespace App\Http\Controllers\Api\Auth;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\RegisterRequest;
 use App\Http\Responses\Api\Response;
-use App\Models\User;
 use App\Services\UserService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     /**
-     * @param Request $request
+     * @param RegisterRequest $request
      * @param UserService $service
      *
      * @return Response
      */
-    public function register(Request $request, UserService $service)
+    public function register(RegisterRequest $request, UserService $service)
     {
-        $data = $request->all();
-        $validator = $this->validator($data);
-
-        if ($validator->fails())
-        {
-            return Response::make()->setStatusCode(422)->addValidationErrors([$validator->errors()]);
-        }
-
-        $user = $service->create($data['name'],  $data['email'],  $data['password']);
+        $user = $service->create($request->name,  $request->email,  $request->password);
 
         $token = $service->createToken($user);
 
         return Response::make(['token' => $token], 200);
-    }
-
-    /**
-     * @param array $data
-     *
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        $messages = [
-            'required' => 'This field is required.',
-        ];
-
-        return Validator::make($data, [
-            'email' => 'required|string|email|max:255|unique:users',
-            'name' => 'required|string|max:255',
-            'password' => 'required|string|min:6|confirmed',
-        ], $messages);
     }
 }
