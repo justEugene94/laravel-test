01b696b60c417b121a71a8870ade6a02555b0656
justEugene94 <drydude77@gmail.com>
2020-11-19 17:45:24 +0200

    Added logic in Api/Auth/LoginController

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -5,11 +5,54 @@ namespace App\Http\Controllers\Api\Auth;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Responses\Api\Response;
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class LoginController extends Controller
 {
-    public function login()
+    /**
+     * @param Request $request
+     * @param UserService $service
+     *
+     * @return Response
+     */
+    public function login(Request $request, UserService $service)
     {
-        return '';
+        $data = $request->all();
+        $validator = $this->validator($data);
+
+        if ($validator->fails())
+        {
+            return Response::make(null, 422)->addValidationErrors([$validator->errors()]);
+        }
+
+        /** @var User $user */
+        $user = User::query()->where('email', $data['email'])->firstOrFail();
+
+        if (!$service->checkPassword($user, $data['password']))
+        {
+            throw new BadRequestHttpException('Wrong email or password');
+        }
+
+        $token = $service->createToken($user);
+
+        return Response::make(['token' => $token], 200);
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
     }
 }
